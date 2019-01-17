3554f3283f15b8e6e3acaf614eedd5079a56f29e
Nikki A <nikki.apostolakis@gmail.com>
2019-01-17 00:34:51 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,9 +1,9 @@
 class CreateStudents < ActiveRecord::Migration
   def change
-    create_table :posts do |t|
-      t.string :title
-      t.text :description
-
+    create_table :students do |t|
+      t.string :first_name
+      t.string :last_name
+      
       t.timestamps null: false
     end
   end
